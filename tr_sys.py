c6e5627ad4324502482f76c7bb033af69fc211c0
isaac-ahlgren <isaacahlgren12@gmail.com>
2022-05-12 04:00:30 +0000

    added pickling

--- tr_sys.py
+++ tr_sys.py
@@ -3,14 +3,18 @@ from network import Network
 import time
 import numpy as np
 
+pickle_folder = "./pickled/"
+
 class TR_Bit_Extract_System():
-    def __init__(self, ip, sample_rate, bit_length, vector_length):
+    def __init__(self, ip, sample_rate, bit_length, vector_length, exp_name):
         self.ip = ip
         self.sample_rate = sample_rate
         self.bit_length = bit_length
         self.vector_length = vector_length
         self.net = Network(ip)
         self.be = Bit_Extractor(sample_rate, bit_length, vector_length)
+        self.exp_name = exp_name
+        self.count = 0
 
     def extract_context(self):
         print()
@@ -33,6 +37,9 @@ class TR_Bit_Extract_System():
             self.get_start()
             key = self.extract_context()
             other_key = self.recieve_auth_token(auth_token)
+	    np.save(pickle_folder + self.exp_name + "_mykey_" + str(self.count) + "_pickled.npy")
+            np.save(pickle_folder + self.exp_name + "_otherkey_" + str(self.count) 
++ "_pickled.npy")
 
     def bit_agreement_exp_host(self):
         while (1):
@@ -41,4 +48,5 @@ class TR_Bit_Extract_System():
             key = self.extract_context()
             time.sleep(1)
             self.net.send_auth_token(auth_token)
+
 
